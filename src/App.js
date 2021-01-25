77575c33d2c7fb7784a6443b53cbd7b1fe51e692
Birger Moell <birger.moell@gmail.com>
2021-01-25 12:11:38 +0100

    Working recorder web socket api

--- src/App.js
+++ src/App.js
@@ -2,24 +2,31 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const socket = new WebSocket('ws://localhost:8080');
+  const startRecording = async () => {
+    const audio = await navigator.mediaDevices.getUserMedia({
+      audio: true,
+      video: false
+    });
+    let mediaRecorder = new MediaRecorder(audio);
+    mediaRecorder.start(50)
+    mediaRecorder.ondataavailable = function(e) {
+      console.log("the mediarecorder data is ", e.data)
+      var data = e.data
+      socket.send(data);
+  }
+}
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button onClick={startRecording}>Record</button>
+       
       </header>
     </div>
   );
 }
+
 
 export default App;
